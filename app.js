07f117b004d83264f7e9f23e1cea03539cc25856
saurabh-kumar@hotmail.com <saurabh.v.kumar@nl.abnamro.com>
2020-09-22 15:39:33 +0200

    install final version

--- app.js
+++ app.js
@@ -68,12 +68,12 @@ if (!config.PG_CONFIG) { //pg config
 if (!config.FB_APP_ID) { //app id
     throw new Error('missing FB_APP_ID');
 }
-if (!config.ADMIN_ID) { //admin id for login
-    throw new Error('missing ADMIN_ID');
-}
-if (!config.FB_PAGE_INBOX_ID) { //page inbox id - the receiver app
-    throw new Error('missing FB_PAGE_INBOX_ID');
-}
+// if (!config.ADMIN_ID) { //admin id for login
+//     throw new Error('missing ADMIN_ID');
+// }
+// if (!config.FB_PAGE_INBOX_ID) { //page inbox id - the receiver app
+//     throw new Error('missing FB_PAGE_INBOX_ID');
+// }
 
 app.set('port', (process.env.PORT || 5000))
 
